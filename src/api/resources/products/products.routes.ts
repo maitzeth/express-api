51b3a4fdb8c72c10078fd5e763137dcf90a68e55
maitzeth <andre801mz@gmail.com>
2024-02-29 12:54:43 -0300

    Refactoring CRUD operations for products

--- src/api/resources/products/products.routes.ts
+++ src/api/resources/products/products.routes.ts
@@ -5,60 +5,66 @@ import { Product } from '../../../types';
 
 const productsRouter = express.Router();
 
-productsRouter.route('/')
-  .get((req: Request, res: Response) => {
-    res.json(products);
-  })
-  .post((req: Request, res: Response) => {
-    const { title, price } = req.body as Product;
+// Get all products
+productsRouter.get('/', (req: Request, res: Response) => {
+  res.json(products);
+});
 
-    if (!title || !price) {
-      return res.status(400).json({ message: 'Invalid request body' });
+// Create a new product
+productsRouter.post('/', (req: Request, res: Response) => {
+  const { title, price } = req.body as Product;
+
+  if (!title || !price) {
+    return res.status(400).json({ message: 'Invalid request body' });
+  }
+
+  // Generate a unique id for this product
+  const id = uuidv4();
+  const newProduct = { id, title, price } satisfies Product;
+
+  res.status(201).json([...products, newProduct]);
+});
+
+// Get the product with the given ID
+productsRouter.get('/:id', (req: Request, res: Response) => {
+  const id = req.params.id;
+  const product = products.find((product) => product.id === id);
+
+  if (product) {
+    return res.json(product);
+  }
+
+  return res.status(404).json({ message: `Product not found on ${id}` });
+});
+
+// Update the product with the given ID
+productsRouter.put('/:id', (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { title, price } = req.body as Product;
+
+  const updatedProducts = products.map((product) => {
+    if (product.id === id) {
+      return {
+        ...product,
+        ...(title && { title }),
+        ...(price && { price }),
+      };
     }
 
-    // Generate a unique id for this product
-    const id = uuidv4();
-    const newProduct = { id, title, price } satisfies Product;
+    return product;
+  });
 
-    res.status(201).json([...products, newProduct]);
-  })
+  res.status(200).json(updatedProducts);
+});
 
-productsRouter.route('/:id')
-  .get((req: Request, res: Response) => {
-    const id = req.params.id;
-    const product = products.find((product) => product.id === id);
-  
-    if (product) {
-      return res.json(product);
-    }
-  
-    return res.status(404).json({ message: `Product not found on ${id}` });
-  })
-  .put((req: Request, res: Response) => {
-    const id = req.params.id;
-    const { title, price } = req.body as Product;
+// Delete the product with the given ID
+productsRouter.delete('/:id', (req: Request, res: Response) => {
+  const id = req.params.id;
 
-    const updatedProducts = products.map((product) => {
-      if (product.id === id) {
-        return {
-          ...product,
-          ...(title && { title }),
-          ...(price && { price }),
-        };
-      }
+  // Devuelvo JSON con producto borrado
+  const deletedProduct = products.find((product) => product.id === id);
 
-      return product;
-    });
-
-    res.status(200).json(updatedProducts);
-  })
-  .delete((req: Request, res: Response) => {
-    const id = req.params.id;
-
-    // Devuelvo JSON con producto borrado
-    const deletedProduct = products.find((product) => product.id === id);
-
-    res.status(200).json(deletedProduct);
-  })
+  res.status(200).json(deletedProduct);
+});
 
 export default productsRouter;
