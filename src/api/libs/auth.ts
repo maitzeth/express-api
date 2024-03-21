bad2e0d55aed904098e060e5b91b883bd7b39eab
maitzeth <andre801mz@gmail.com>
2024-03-20 21:00:28 -0300

    WIP: Implement JwtStrategy for user authentication in auth.ts

--- src/api/libs/auth.ts
+++ src/api/libs/auth.ts
@@ -1,5 +1,22 @@
 import { users } from '@/database'; 
 import bcrypt from 'bcrypt';
+import { Strategy } from 'passport-jwt';
+import { logger } from '@/utils/logger';
+
+const jwtOptions = {
+  
+}
+
+const JwtStrategy = new Strategy({}, (jwtPayload, done) => {
+  const user = users.find((user) => user.id === jwtPayload.id);
+
+  if (user) {
+    return done(null, user);
+  }
+
+  logger.info(`User not found: ${jwtPayload.id}`);
+  return done(null, false);
+});
 
 export default (username: string, password: string, done: (error: any, user?: any) => void) => {
   const user = users.find((user) => user.username === username);
