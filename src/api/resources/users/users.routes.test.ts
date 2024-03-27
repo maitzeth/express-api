b0e736b8e6098ac3dd0db817d6ab776538160a60
maitzeth <andre801mz@gmail.com>
2024-03-26 23:12:17 -0300

    adding more users tests

--- src/api/resources/users/users.routes.test.ts
+++ src/api/resources/users/users.routes.test.ts
@@ -4,23 +4,46 @@ import { app, server } from '../../../../app';
 import Users from '@src/api/resources/users/users.model';
 import { describe, expect, test } from '@jest/globals';
 
+let dummyUsers = [
+  {
+    username: 'user1',
+    email: 'user1@mail.com',
+    password: "password123",
+  },
+  {
+    username: 'user2',
+    email: 'user1@mail.com',
+    password: "password123",
+  }
+];
+
+
 describe('Users Routes', () => {
-  beforeEach((done) => {
-    Users.deleteMany();
-    done();
+  beforeEach(async () => {
+    await Users.deleteMany({ });
   });
 
-  afterAll(() => {
+  afterAll(async () => {
+    await Users.deleteMany({ });
     server.close();
   });
 
   describe('GET /users', () => {
-    test('it should return a list of users', async () => {
+    test('it should return empty list of users', async () => {
         const response = await request(app).get('/users');
 
         expect(response.status).toBe(200);
         expect(response.body).toEqual([]);
         expect(response.body).toHaveLength(0);
+    });
+
+    test('it should return a list of users', async () => {
+      await Promise.all(dummyUsers.map(user => Users.create(user)));
+
+      const response = await request(app).get('/users');
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveLength(2);
     });
   });
 });
